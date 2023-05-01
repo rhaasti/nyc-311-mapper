50b9b4b31a6f91d127c7977a9b2929d131f004ac
ryan h <rhaasti@gmail.com>
2023-05-01 17:02:21 -0400

    merge conflict

--- controllers/partyController.js
+++ controllers/partyController.js
@@ -3,39 +3,11 @@ const Party = require('../models/partyModel');
 
 console.log('in the controller');
 
-const DbController = {}
-
-DbController.findByBoroughAndDay = (req, res, next) => {
-    console.log("inside findByBoroughAndDay")
-
-
-    Party.find({ borough: req.body.borough, createdDate: req.body.day })
-        .then((locations) => {
-            res.locals.locations = locations
-            return next()
-        })
-        .catch((err) => console.log(err))
-},
-
-    DbController.testGet = (req, res, next) => {
-
-        res.locals.message = "res.body on testGet"
-        console.log("testGet")
-        return next()
-
-        if (err) {
-            return next(err)
-        }
-        next()
-
-    }
 
 async function callToAPI() {
-
-
     try {
         console.log('in the try block')
-        const response = await fetch("https://data.cityofnewyork.us/resource/erm2-nwe9.json?descriptor='Loud Music/Party'&$limit=10000&$select=unique_key,created_date,incident_address,incident_zip,borough,descriptor,complaint_type,latitude,longitude&$$app_token=h1WcLxGB20iLGSrjy1z7bt3Kn");
+        const response = await fetch("https://data.cityofnewyork.us/resource/erm2-nwe9.json?descriptor='Loud Music/Party'&$limit=1000&$select=unique_key,created_date,incident_address,incident_zip,borough,descriptor,complaint_type,latitude,longitude&$$app_token=h1WcLxGB20iLGSrjy1z7bt3Kn");
 
         const data = await response.json();
         data.forEach((record) => {
@@ -54,6 +26,7 @@ async function callToAPI() {
                 complaintType: record.complaint_type,
                 latitude: record.latitude,
                 longitude: record.longitude
+                // }).catch(err)
             }).catch((err) => console.log('error'))
         })
     }
@@ -65,6 +38,26 @@ async function callToAPI() {
 
 callToAPI();
 
+const DbController = {}
+
+DbController.findByBoroughAndDay = (req, res, next) => {
+    if (req.body.borough === "ALL") {
+        Party.find({ createdDate: req.body.day })
+            .then((locations) => {
+                res.locals.locations = locations
+                return next()
+            })
+            .catch((err) => console.log(err))
+    }
+    else {
+        Party.find({ borough: req.body.borough, createdDate: req.body.day })
+            .then((locations) => {
+                res.locals.locations = locations
+                return next()
+            })
+            .catch((err) => console.log(err))
+    }
+}
+
 
 module.exports = DbController;
-
